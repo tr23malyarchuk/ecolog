2ef504e688c4c9f511564bb68526babccd4c5d7a
tr23malyarchuk <123400836+tr23malyarchuk@users.noreply.github.com>
2024-10-10 14:00:22 +0000

    Added Docker Compose in order to manage Docker configuration

--- server.js
+++ server.js
@@ -1,39 +1,16 @@
-const express = require('express');
 const mysql = require('mysql2');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',  
-  password: 'yhhg5bfbhun', 
-  database: 'mydb'  
+    host: 'localhost',
+    user: 'root',
+    password: 'Av_s0l*YaM4)2005',
+    database: 'ecolog'
 });
 
-connection.connect(err => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
-app.get('/api/data', (req, res) => {
-  const query = 'SELECT * FROM обєкт';
-
-  connection.query(query, (err, results) => {
+connection.connect((err) => {
     if (err) {
-      console.error('Query executing error:', err);
-      res.status(500).send('Database error');
-      return;
+        console.error('Connection error: ' + err.stack);
+        return;
     }
-    res.json(results);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+    console.log('Connected as id ' + connection.threadId);
 });
