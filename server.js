d9b1cf6832c3165323c7b4d71c3baa0afa173a91
tr23malyarchuk <123400836+tr23malyarchuk@users.noreply.github.com>
2024-10-10 13:06:04 +0000

    Configured .gitignore file

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'mydbuser',
-  password: 'mydb',
-  database: 'mydb'
+  user: 'root',  
+  password: 'yhhg5bfbhun', 
+  database: 'mydb'  
 });
 
 connection.connect(err => {
   if (err) {
-    console.error('Error connection to MySQL database:', err);
+    console.error('Error connecting to the database:', err);
     return;
   }
-  console.log('Connected to MySQL');
+  console.log('Connected to MySQL database');
 });
 
 app.get('/api/data', (req, res) => {
-  const query = 'SELECT * FROM mydb.обєкт';
+  const query = 'SELECT * FROM обєкт';
 
   connection.query(query, (err, results) => {
     if (err) {
@@ -26,4 +31,9 @@ app.get('/api/data', (req, res) => {
     }
     res.json(results);
   });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
